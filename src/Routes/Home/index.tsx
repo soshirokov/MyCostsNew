1110215de40669dde98e2bf7cb6133c071429cd3
soshirokov <soshirokov@gmail.com>
2022-08-14 12:27:09 +0300

    CategorySetting component

--- src/Routes/Home/index.tsx
+++ src/Routes/Home/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import NewCalendar from '../../Components/Calendar';
+import { CategoriesSetting } from '../../Components/CategoriesSetting';
 
 const Home = () => {
     return (
         <div className='home'>
             <NewCalendar />
+
+            <CategoriesSetting />
         </div>
     );
 };

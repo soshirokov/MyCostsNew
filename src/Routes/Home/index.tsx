f867e5d21ea6dc49be0f9a8cd5a1145cb7470e83
soshirokov <soshirokov@gmail.com>
2022-08-15 17:30:40 +0300

    Main Page

--- src/Routes/Home/index.tsx
+++ src/Routes/Home/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import { Col, Row, Typography } from 'antd';
 import NewCalendar from '../../Components/Calendar'
+import { AddCosts } from '../../Components/AddCosts';
 
-
+const { Title } = Typography
 
 const Home = () => {
     return (
         <div className='Home'>
-            <NewCalendar />
+            <Title level={2}>Your today costs</Title>
+            <Row>
+                <Col span={8}><NewCalendar /></Col>
+                <Col span={8}><AddCosts /></Col>
+                <Col span={8}></Col>
+            </Row>
+            
         </div>
     );
 };

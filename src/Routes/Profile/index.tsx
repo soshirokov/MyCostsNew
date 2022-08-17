cdf1b45df0c99290a29b2ed91ce863d25fc2bd3c
Vladimir <vladimir@yakimkov.ru>
2022-08-17 19:11:20 +0300

    Add Routes Prof

---
+++ src/Routes/Profile/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Profile } from '../../Components/Profile';
+
+const Profile = () => {
+    return (
+        <div className='profile'>
+            <Profile />
+        </div>
+    );
+};
+
+export { Profile };
